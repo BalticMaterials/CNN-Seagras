058f986a38e557639925c053be4f58af4a59d3c9
Taenar97 <sven1997@gmx.net>
2023-04-26 08:05:42 +0200

    deleted utility function for folder renaming

--- runs/Apr25_12-37-09_balticmaterials-MS-7B98/rename.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path = "./runs/Apr25_12-37-09_balticmaterials-MS-7B98/"
-for x in os.listdir(path):
-    if " " in x:
-        os.rename(path + x, path + x.replace(" ", "_"))
-        # print(F"FILE TO REPLACE{x}")
-    else:
-        print(x)
