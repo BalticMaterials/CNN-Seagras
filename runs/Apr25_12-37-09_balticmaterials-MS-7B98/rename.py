ad42af7cfd6b6a64faf3aa0354295881307b5252
tjark <ziehm@balticmaterials.de>
2023-04-26 08:01:32 +0200

    changed folder naming to microsoft conform

--- runs/Apr25_12-37-09_balticmaterials-MS-7B98/rename.py
+++ runs/Apr25_12-37-09_balticmaterials-MS-7B98/rename.py
@@ -1,7 +1,9 @@
 import os
 
-for x in os.listdir("./"):
-    if ":" in x:
-        os.rename(x, x.replace(":", "_"))
+path = "./runs/Apr25_12-37-09_balticmaterials-MS-7B98/"
+for x in os.listdir(path):
+    if " " in x:
+        os.rename(path + x, path + x.replace(" ", "_"))
+        # print(F"FILE TO REPLACE{x}")
     else:
         print(x)
