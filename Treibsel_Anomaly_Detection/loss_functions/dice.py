62e1dd027739fa1e90c7e17996e99ec651e2bf31
Taenar97 <sven1997@gmx.net>
2023-05-02 13:42:30 +0200

    Updated Modell metrics and loss

--- Treibsel_Anomaly_Detection/loss_functions/dice.py
+++ Treibsel_Anomaly_Detection/loss_functions/dice.py
@@ -6,18 +6,13 @@ class DiceLoss(nn.Module):
         super(DiceLoss, self).__init__()
 
     def forward(self, inputs, targets, smooth=1):
-        
-        #comment out if your model contains a sigmoid or equivalent activation layer
-        # inputs = F.sigmoid(inputs)       
-        
-        #flatten label and prediction tensors
+
+        inputs = F.sigmoid(inputs)
+
         inputs = inputs.view(-1)
         targets = targets.view(-1)
-        
-        intersection = (inputs * targets).sum()                            
-        dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
-        
+
+        intersection = (inputs * targets).sum()
+        dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)
+
         return 1 - dice
-    
-    def get_name() -> str:
-        return "DiceLoss"
