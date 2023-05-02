90b0dfb7f8d3bba3b1f0e8a2327929399e51e9d2
Taenar97 <sven1997@gmx.net>
2023-05-02 07:59:25 +0200

    metric eval

--- Treibsel_Anomaly_Detection/train.py
+++ Treibsel_Anomaly_Detection/train.py
@@ -23,19 +23,10 @@ from utils import (
 
 
 # Hyperparameters etc.
-RUN_NAME = "BCEWithLogitsLoss, Batch: 2, LR: 1e-2"
-LEARNING_RATE = 1e-3
-rates = [1e-4, 1e-3, 1e-2]
-
-from loss_functions.dice import DiceLoss
-from loss_functions.IoU import IoULoss
-from loss_functions.tversky import TverskyLoss, FocalTverskyLoss
-loss_functions = [nn.BCEWithLogitsLoss(), DiceLoss(), IoULoss(), TverskyLoss(), FocalTverskyLoss()]
-LOSS_FN = loss_functions[0]
-
+RUN_NAME = "BCEWithLogitsLoss"
+LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 2
-batches = [2, 4, 8, 16, 32]
 NUM_EPOCHS = 50
 NUM_WORKERS = 2
 IMAGE_HEIGHT = 160 # changing size later in the training process to increase accuracy, then resize with nearest interpolation
@@ -96,57 +87,52 @@ def main():
         ),
         ToTensorV2(),
     ],)
+   
+            
+    logging.info(f"Start of run: {RUN_NAME}")
 
+    model = UNET(in_channels=3, out_channels=1).to(DEVICE)
 
-    for loss_function in loss_functions:
-        for batch_size in batches:
-            for lr in rates:
-                # TODO: Reset model and make new training
-                RUN_NAME = f"{loss_function._get_name()}, Batch: {batch_size}, LR: {lr}"
-                LOSS_FN = loss_function
-                BATCH_SIZE = batch_size
-                LEARNING_RATE = lr
+    from loss_functions.dice import DiceLoss
+    from loss_functions.IoU import IoULoss
+    from loss_functions.tversky import TverskyLoss, FocalTverskyLoss    
+    loss_fn = nn.BCEWithLogitsLoss()
 
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-                logging.info(f"Star of run: {RUN_NAME}")
-                model = UNET(in_channels=3, out_channels=1).to(DEVICE)    
-                loss_fn = LOSS_FN
-                
-                optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    train_loader, val_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        BATCH_SIZE,
+        train_transform,
+        val_transform,
+        NUM_WORKERS,
+        PIN_MEMORY
+    )
 
-                train_loader, val_loader = get_loaders(
-                    TRAIN_IMG_DIR,
-                    TRAIN_MASK_DIR,
-                    VAL_IMG_DIR,
-                    VAL_MASK_DIR,
-                    BATCH_SIZE,
-                    train_transform,
-                    val_transform,
-                    NUM_WORKERS,
-                    PIN_MEMORY
-                )
+    if LOAD_MODEL:
+        load_checkpoint(torch.load(PATH + "data/my_checkpoint.pth.tar"), model)
 
-                if LOAD_MODEL:
-                    load_checkpoint(torch.load(PATH + "data/my_checkpoint.pth.tar"), model)
-                
-                scaler = torch.cuda.amp.GradScaler()
-                for epoch in range(NUM_EPOCHS):
-                    loss = train_fn(train_loader, model, optimizer, loss_fn, scaler, epoch)
+    scaler = torch.cuda.amp.GradScaler()
 
-                    # save model
-                    if epoch == 49:
-                        checkpoint = {
-                            "state_dict": model.state_dict(),
-                            "optimizer": optimizer.state_dict(), 
-                        }
-                        save_checkpoint(checkpoint)
-                        save_predictions_as_imgs(val_loader, model, folder="saved_images/", device=DEVICE)
+    for epoch in range(NUM_EPOCHS):
+        loss = train_fn(train_loader, model, optimizer, loss_fn, scaler, epoch)
+        # save model
+        
+        checkpoint = {
+            "state_dict": model.state_dict(),
+            "optimizer": optimizer.state_dict(), 
+        }
+        save_checkpoint(checkpoint)
+        save_predictions_as_imgs(val_loader, model, folder="saved_images/", device=DEVICE)
 
-                    metrics = generate_metrics(val_loader, model, device=DEVICE)
-                    metrics.update({"loss": loss})
-                    write_metrics_to_TensorBoard(metrics, epoch, run_name=RUN_NAME)                        
-                
-                logging.info(f"End of run: {RUN_NAME}")
+        metrics = generate_metrics(val_loader, model, device=DEVICE)
+        metrics.update({"loss": loss})
+        write_metrics_to_TensorBoard(metrics, epoch, run_name=RUN_NAME)                        
+       
+    logging.info(f"End of run: {RUN_NAME}")
 
 if __name__ == "__main__":
     main()
